7193194d4e6ceb74a6e3b77f571a1071e87a8c0a
soo <onemask@naver.com>
2021-11-17 01:13:52 +0900

    using CarsFactory

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
-import step3.Cars
+import step3.CarsFactory
 import step3.Round
 import step3.ui.Input
 import step3.ui.Output
 
 fun main() {
-    val cars = Cars()
-    cars.makeCar(numberOfCount = Input.getCountOfCar())
-
+    val cars = CarsFactory.makeCars()
     val rond = Round(number = Input.getNumberOfRound())
     Output.printResult(cars = cars, round = rond)
 }
